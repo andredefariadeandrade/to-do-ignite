a1b371d9b346d9801a077322a4b56409f9e7c632
andredefariadeandrade <andre.faria.andrade@gmail.com>
2021-10-14 14:45:34 -0300

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4, validate } = require('uuid');
 
 const app = express();
 
@@ -13,11 +13,55 @@ const users = [];
 function checksExistsUserAccount(request, response, next) {
   const { username } = request.headers;
 
-  const usuario = users.find(usuario => usuario.username === username);
+  if(!username) return response.status(400).json({ error: 'username is required' });
 
-  if(!usuario){
-    return response.status(404).json({ message: "Username not found. "})
+  const usuario = users.find((usuario) => usuario.username === username);
+  if (!usuario) return response.status(404).json({ error: 'user not exists' });
+
+  request.usuario = usuario;
+
+  return next();
+}
+
+function checksCreateTodosUserAvailability(request, response, next) {
+  const { usuario } = request;
+
+  if (!usuario.pro && usuario.todos.length < 10) {
+    return next();
+  } else if (usuario.pro) {
+    return next();
+  } else {
+    return response.status(403).json({ error: 'limit reached' });
   }
+}
+
+function checksTodoExists(request, response, next) {
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const usuario = users.find((usuario) => usuario.username === username);
+  if (!usuario) return response.status(404).json({ error: 'user not found' });
+
+  const checkUuidIsValid = validate(id);
+  if (!checkUuidIsValid) return response.status(400).json({ error: 'Id invalid' });
+
+  const todo = usuario.todos.find(todo => todo.id === id);
+  if (!todo) return response.status(404).json({ error: 'todo not found' });
+  
+  request.todo = todo;
+  request.usuario = usuario;
+
+  return next();
+}
+
+function findUserById(request, response, next) {
+  const { id } = request.params;
+
+  if (!id) return response.status(400).json({ error: 'id is required' });
+
+  const usuario = users.find((usuario) => usuario.id === id);
+  if (!user) return response.status(404).json({ error: 'user not found' });
+
   request.usuario = usuario;
 
   return next();
@@ -102,22 +146,29 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  const { usuario } = request;
-
+  const { usuario, todo } = request;
   const { id } = request.params;
+  
+  const todoIndex = usuario.todos.indexOf(todo);
 
-  const todoIndex = usuario.todos.findIndex(todo => todo.id === id);
-
-  if(todoIndex === -1){
-    return response.status(404).json({ error: "id not found. "})
+  if (todoIndex === -1) {
+    return response.status(404).json({ error: 'Todo not found' });
   }
 
-  users.todos.splice(todoIndex, -1);
+  usuario.todos.splice(todoIndex, 1);
 
   return response.status(204).send();
 });
 
 app.listen(3333, () => {
   console.log('Servidor On localhost:3333. ');
-})
-module.exports = app;
+});
+
+module.exports = {
+  app,
+  users,
+  checksExistsUserAccount,
+  checksCreateTodosUserAvailability,
+  checksTodoExists,
+  findUserById
+};
